3cc368f1b4546aa32076fd6ec88c61cd4b15c274
Choon Gee <cgee.code@gmail.com>
2024-09-02 21:09:37 +0800

    designing the home route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
+  template: `
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
+  styles: ``,
 })
 export class AppComponent {
   title = 'lemon-mart'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router'
 
-export const routes: Routes = []
+import { HomeComponent } from './home/home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+]
