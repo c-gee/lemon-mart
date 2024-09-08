58cda93a120a5f77ab941f7e3d287c2ce785cac1
Choon Gee <cgee.code@gmail.com>
2024-09-08 17:04:03 +0800

    an http interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
+import { AuthHttpInterceptor } from './auth/auth.http.interceptor'
 import { InMemoryAuthService } from './auth/auth.in-memory.service'
 import { AuthService } from './auth/auth.service'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([AuthHttpInterceptor])),
     provideRouter(routes),
     provideAnimationsAsync(),
     {
